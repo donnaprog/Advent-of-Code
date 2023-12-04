55339636ee1b8b0f732cc1a6e5ac1ce56c5333b1
d.l.molle@ind.nl <d.l.molle@ind.nl>
2023-12-04 17:01:54 +0100

    Complete day 3 part 1

--- code/day_3.py
+++ code/day_3.py
@@ -2,10 +2,11 @@ import numpy as np
 import re
 
 # Read as 2D ndarray
-with open('../input/puzzle_input_day_3_example.txt') as f:
+with open('../input/puzzle_input_day_3.txt') as f:
     puzzle_input = [list(line)[:-1] for line in f.readlines()]
     mat = np.array(puzzle_input)
 
+# Part 1
 # Find symbol coordinates
 symbols = set()
 symbol_locations = set()
@@ -14,13 +15,19 @@ for coords, char in np.ndenumerate(mat):
         symbols.add(char)
         symbol_locations.add(coords)
 
+
+def find_box_coordinates(r, start, end):
+    box_coords = set((y, x) for y in [r - 1, r, r + 1] for x in range(start - 1, end + 1))
+    return box_coords
+
+
 sum_numbers = 0
 for r, row in enumerate(mat):
     line = ''.join(row)
     res = re.finditer('\d+', line)
     for match in res:
-        span = list(range(match.span()[0], match.span()[1]))
-        box_coordinates_set = set((y, x) for y in [r-1, r, r+1] for x in list(range(span[0], span[-1] +2)))
+        start, end = match.start(), match.end()
+        box_coordinates_set = find_box_coordinates(r, start, end)
         if symbol_locations & box_coordinates_set:
             sum_numbers += int(match.group())
 
