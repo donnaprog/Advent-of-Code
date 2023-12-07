3cb4a395d013225dcc66e9988fab538e55ff03a9
d.l.molle@ind.nl <d.l.molle@ind.nl>
2023-12-07 08:28:32 +0100

    Complete day 3 part 2

--- code/day_3.py
+++ code/day_3.py
@@ -22,13 +22,42 @@ def find_box_coordinates(r, start, end):
 
 
 sum_numbers = 0
+numbers = []
+number_coords = []
 for r, row in enumerate(mat):
     line = ''.join(row)
     res = re.finditer('\d+', line)
+    line_numbers = []
+    line_coords = []
     for match in res:
+        line_numbers.append(int(match.group()))
         start, end = match.start(), match.end()
+        line_coords.append([(r, xn) for xn in range(start, end)])
         box_coordinates_set = find_box_coordinates(r, start, end)
         if symbol_locations & box_coordinates_set:
             sum_numbers += int(match.group())
+    numbers.append(line_numbers)
+    number_coords.append(line_coords)
 
 print(sum_numbers)
+
+# Part 2
+gear_locations = set()
+for coords, char in np.ndenumerate(mat):
+    if char == '*':
+        gear_locations.add(coords)
+
+gear_ratio_sum = 0
+for gear_loc in gear_locations:
+    neighbours = find_box_coordinates(gear_loc[0], gear_loc[1], gear_loc[1] + 1)
+    factors = []
+    for line_nr, values in enumerate(numbers):
+        for pos, value in enumerate(values):
+            coords = set(number_coords[line_nr][pos])
+            if neighbours & coords:
+                factors.append(numbers[line_nr][pos])
+    if len(factors) == 2:
+        gear_ratio_sum += factors[0]*factors[1]
+
+print(gear_ratio_sum)
+
